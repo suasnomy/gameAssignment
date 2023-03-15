16e8b193903619781ec1095ecb672f40f3112a7f
suasnomy <bhandarisudhir4274@gmail.com>
2023-03-15 20:57:37 +0530

    added upto video 3

---
+++ My project 2/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    private Transform lookAt;
+    private Vector3 startOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+        startOffset = transform.position - lookAt.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = lookAt.position + startOffset;
+    }
+}
---
+++ My project 2/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 0.5f;
+    private Vector3 moveVector;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = Vector3.zero;
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else {
+            verticalVelocity -= gravity * Time.deltaTime; 
+        }
+        //calculate x , y and z. x is left and right, y is up and down , z is forward and backward.
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
